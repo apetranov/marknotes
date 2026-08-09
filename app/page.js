e1182cc81ae94528bc7aac3efdd57e9b6bacf9e6
Alexander Petranov <shasho68.5@gmail.com>
2026-08-09 13:34:37 +0300

    First commit

--- app/page.js
+++ app/page.js
@@ -1,69 +1,32 @@
+'use client'
 import Image from "next/image";
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@base-ui/react";
+import { useState, useEffect } from "react";
+import markdown from "@wcj/markdown-to-html";
+import parse from "html-react-parser"
+import Link from "next/link";
 
 export default function Home() {
+  const [markdownTxt, setMarkdownTxt] = useState('');
+  const [mk, setMk] = useState('');
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.js
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="flex flex-col justify-center items-center p-10 gap-10">
+      <h1 className="font-black text-7xl"><span className="text-green-600">Mark</span>Notes</h1>
+      <h3>Check markdown syntax <Link className="text-blue-500 underline" target="_blank" href={"https://www.markdownguide.org/cheat-sheet/"}>here</Link></h3>
+      <h2 className="text-4xl">Note preview</h2>
+      <div className="shadow-xl p-10 outline-1 outline-green-600 markdown-body">
+          {parse(mk)}
+      </div>
+      <Textarea 
+        value={markdownTxt}
+        onChange={(e) => {
+          setMarkdownTxt(e.target.value);
+          setMk(markdown(e.target.value));
+        }}
+      />
+      <Button className="bg-black text-white py-1 px-4 rounded-2xl">+ Create Note</Button>
     </div>
   );
 }
